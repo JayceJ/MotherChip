b3bdc181556d92582971fd1f397f96f565201c8e
JayceJ <jayce.jordan@gmail.com>
2013-06-13 14:46:16 +1200

    Modified the SQL and menu view along with the css.
    Also added in a few images for testing

--- includes/menu_view.php
+++ includes/menu_view.php
@@ -18,13 +18,13 @@ class Menu_View{
 			$current_type = $types[$i];
 			$products = $current_type->products;
 
-			$html .= '<li><a href="type_page.php?typeID='.$current_type->ID.'">'.$current_type->typeName.'</a></li> <ul>';
+			$html .= '<div class="navType"><li><a href="type_page.php?typeID='.$current_type->ID.'">'.$current_type->typeName.'</a></li></div> <ul>';
 
 			for($j = 0; $j < count($products); $j++){
 
 				$current_product = $products[$j];
 
-				$html .= '<li><a href="product_page.php?productID='.$current_product->ID.'">'.$current_product->productName.'</a></li>';
+				$html .= '<div class="navProduct"><li><a href="product_page.php?productID='.$current_product->ID.'">'.$current_product->productName.'</a></li></div>';
 
 			}
 
--- motherchip.sql
+++ motherchip.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 10, 2013 at 12:10 p.m.
+-- Generation Time: Jun 13, 2013 at 02:40 p.m.
 -- Server version: 5.1.41
 -- PHP Version: 5.3.1
 
@@ -36,14 +36,17 @@ CREATE TABLE IF NOT EXISTS `tbcustomer` (
   `Password` varchar(100) NOT NULL,
   `Credit` int(11) NOT NULL,
   PRIMARY KEY (`CustomerID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `tbcustomer`
 --
 
 INSERT INTO `tbcustomer` (`CustomerID`, `FirstName`, `LastName`, `Address`, `Telephone`, `Email`, `UserName`, `Password`, `Credit`) VALUES
-(1, 'Nathaniel', 'Smith', '1 Ilivehereatthis place', 123456789, 'nat@notmyemail.com', 'nathaniel.smith', 'thisismypassword', 10);
+(1, 'Nathaniel', 'Smith', '1 Ilivehereatthis place', 123456789, 'nat@notmyemail.com', 'nathaniel.smith', 'thisismypassword', 10),
+(3, 'Test', 'Ificate', '1 thisismyaddress street', 987654321, 'test@myemail.com', 'Test.Ificate', 'hihihi', 80),
+(4, 'Jayce', 'Jordan', '1 ithinkilivehere road', 980723489, 'jayce.jordan@nomail.com', 'jayce.jordan', 'thisisapassword', 9001),
+(5, 'Enzo', 'Something', '1 thisisenzosroad street', 234534534, 'enzo.something@nomail.com', 'enzo.something', 'thisismypasswordforenzo', 1);
 
 -- --------------------------------------------------------
 
@@ -108,14 +111,31 @@ CREATE TABLE IF NOT EXISTS `tbproduct` (
   `Active` int(11) NOT NULL,
   PRIMARY KEY (`ProductID`),
   KEY `TypeID` (`TypeID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
 
 --
 -- Dumping data for table `tbproduct`
 --
 
 INSERT INTO `tbproduct` (`ProductID`, `ProductName`, `Description`, `Price`, `TypeID`, `StockLevel`, `PhotoPath`, `Active`) VALUES
-(1, 'Super Gweefix Card', 'The best ever.', 1500, 1, 10, 'testingdb.jpg', 1);
+(1, 'Super Gweefix Card', 'The best ever.', 1500, 1, 10, 'testingdb.jpg', 1),
+(2, 'Worst MuthaBoard', 'The worst one ever.', 200, 2, 200, 'worstmotherboard.jpg', 1),
+(5, 'Corsair RAM', 'The most expensive ram possible', 200, 5, 20, 'corsairram.jpg', 1),
+(6, 'Cooler Master RAM', 'Slightly less expensive RAM', 150, 5, 30, 'coolermasterram.jpg', 1),
+(7, 'Literal RAM', 'This is actually a real ram', 175, 5, 3, 'thisisaram.jpg', 1),
+(8, 'Nvidia GTX Titan', 'Ultimate GFX card  ', 1900, 1, 6, 'titan.jpg', 1),
+(9, 'Radeon HD 7990', 'Ultimate GFX card', 1900, 1, 6, '7990.jpg', 1),
+(10, 'Antec Eleven Hundred', 'Simple Antec case with nice cable management', 200, 3, 3, '1100.jpg', 1),
+(11, 'CoolerMaster Cosmos II', 'Ultimate Ultra Tower case', 700, 3, 2, 'cosmos.jpg', 1),
+(12, 'Storm Scout II', 'CoolerMaster branded. Nice high end case', 200, 3, 2, 'scout.jpg', 1),
+(13, 'ASUS Z77 Sabertooth', 'Great high end motherboard', 360, 2, 4, 'sabertooth.jpg', 1),
+(14, 'Gigabyte Z77X-UD3H', 'Mit to high end motherboard supports 1155 intel CPU''s', 240, 2, 5, 'gigabytez77.jpg', 1),
+(15, 'Intel i7-3770K', 'High end i7 CPU. Unlocked', 450, 4, 8, 'i7.jpg', 1),
+(16, 'Intel i5-3570', 'High end i5 CPU. Not unlocked', 300, 4, 8, 'i5.jpg', 1),
+(17, 'Intel i5-4670K (Haswell)', 'New Haswell line of CPU. Ulocked. A step up from Ivy Bridge', 380, 4, 7, 'haswell.jpg', 1),
+(18, 'Corsair Hydro H100i', 'Closed loop CPU cooling with dual 120mm radiator', 200, 6, 8, 'h100i.jpg', 1),
+(19, 'Antec big Boy 200', 'Giant 200mm exhaust fan compatible with most cases.', 50, 6, 9, 'antecbb.jpg', 1),
+(20, 'Sickleflow 120 (green)', 'Green LEd 120mm fans. Great for intake. ~65CFM', 25, 6, 20, 'sickleflow', 1);
 
 -- --------------------------------------------------------
 
@@ -129,14 +149,19 @@ CREATE TABLE IF NOT EXISTS `tbproducttype` (
   `Description` varchar(300) NOT NULL,
   `DisplayOrder` int(11) NOT NULL,
   PRIMARY KEY (`TypeID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `tbproducttype`
 --
 
 INSERT INTO `tbproducttype` (`TypeID`, `TypeName`, `Description`, `DisplayOrder`) VALUES
-(1, 'Graphics Card', 'Run stuff really fast with super expensive graphics hardware', 1);
+(1, 'Graphics Card', 'Run stuff really fast with super expensive graphics hardware', 1),
+(2, 'Motherboard', 'For pluggin all ya stuff.', 2),
+(3, 'Chassis', 'Computer cases of all sizes', 3),
+(4, 'Processors', 'The processor for the computer', 4),
+(5, 'Memory', 'Random access memory', 5),
+(6, 'Cooling', 'Fans and water cooling', 6);
 
 --
 -- Constraints for dumped tables
@@ -152,8 +177,8 @@ ALTER TABLE `tborder`
 -- Constraints for table `tborderline`
 --
 ALTER TABLE `tborderline`
-  ADD CONSTRAINT `tborderline_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `tbproduct` (`ProductID`),
-  ADD CONSTRAINT `tborderline_ibfk_1` FOREIGN KEY (`OrderID`) REFERENCES `tborder` (`OrderID`);
+  ADD CONSTRAINT `tborderline_ibfk_1` FOREIGN KEY (`OrderID`) REFERENCES `tborder` (`OrderID`),
+  ADD CONSTRAINT `tborderline_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `tbproduct` (`ProductID`);
 
 --
 -- Constraints for table `tbproduct`
